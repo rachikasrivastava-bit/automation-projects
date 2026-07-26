7450924891d244f844c31a93f4f32871fdda2d2b
rachikasrivastava-bit <rachika.srivastava@acldigital.com>
2026-07-26 21:47:55 +0530

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from playwright.sync_api import sync_playwright, Page
+
+@pytest.fixture()
+
+def login_session():
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=False, slow_mo=500)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto("https://www.saucedemo.com/")
+        page.fill("#user-name", "standard_user")
+        page.fill("#password", "secret_sauce")
+        page.click("#login-button")
+        page.wait_for_timeout(3000)
+        page.wait_for_url("**/inventory.html")
+        page.click("#react-burger-menu-btn")
+        page.click("#logout_sidebar_link")
+
+        yield page
+
+        
+        context.close()
+        browser.close()
---
+++ loginfixture.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import expect
+
+# Simply pass the fixture names from conftest.py as arguments
+def test_verify_dashboard_message(login_session):
+    
+    # app_logo = login_session.locator(".header_label")
+    
+    # assert app_logo.text_content() == "Swag Labs"
+
+    expect(login_session.locator(".header_label")).to_have_text("Swag Labs")
